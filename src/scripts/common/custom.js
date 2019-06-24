eaefccd83a15a0c92034f95d94b0db5a51f4da61
MrVolKing <99519851vvvk@gmail.com>
2019-06-24 14:45:52 +0300

    mobile

--- src/scripts/common/custom.js
+++ src/scripts/common/custom.js
@@ -3,20 +3,20 @@ $(document).ready(() => {
 
     // burger
     $('.burger').click(function() {
-        $('.adaptmenu').addClass('adaptmenu_active');
-        $('body').css('overflow', 'hidden');
+        $('.adaptmenu').toggleClass('adaptmenu_active');
+        // $('body').css('overflow', 'hidden');
     });
 
-    $('.close').click(function() {
-        $('.adaptmenu').removeClass('adaptmenu_active');
-        $('body').css('overflow', 'visible');
-        $('body').css('overflow-x', 'hidden');
-    });
+    // $('.close').click(function() {
+    //     $('.adaptmenu').removeClass('adaptmenu_active');
+    //     $('body').css('overflow', 'visible');
+    //     $('body').css('overflow-x', 'hidden');
+    // });
 
     $('.adaptmenu__item').click(function() {
         $('.adaptmenu').removeClass('adaptmenu_active');
-        $('body').css('overflow', 'visible');
-        $('body').css('overflow-x', 'hidden');
+        // $('body').css('overflow', 'visible');
+        // $('body').css('overflow-x', 'hidden');
     });
 
 
@@ -81,8 +81,9 @@ $(document).ready(() => {
         loop: false,
         slidesPerView: 3,
         centeredSlides: false,
+        noSwiping: true,
         breakpoints: {
-            769: {
+            500: {
                 slidesPerView: 1,
                 pagination: {
                     el: '.swiper-pagination',
@@ -95,11 +96,54 @@ $(document).ready(() => {
         }
     });
 
-    
-
 });
 
-                                                                        
+
+
+// faq
+
+$(document).on('click', '.questionitem', function(){
+    console.log('da');
+    $('.questionitem__bottom').addClass('hidden');
+    $(this).find('.questionitem__bottom').removeClass('hidden');
+    $('.questionitem__icon img').removeClass('trans_rot');
+    $(this).find('.questionitem__icon img').addClass('trans_rot');
+    // $('.img2').addClass('hidden');
+    // $('.img1').removeClass('hidden');
+    // $(this).find('.img2').removeClass('hidden');
+    // $(this).find('.img1').addClass('hidden');
+    
+});
+
+
+
+
+// select
+
+$('.select').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('expanded');
+    $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+
+$(document).click(function() {
+    $('.select').removeClass('expanded');
+});
+
+      
+
+//scroll one screen bottom
+
+$(document).ready(function(){
+    $('.scroll_bottom').click(function () {
+        var win = $(window).height();
+        $('body,html').animate({
+            scrollTop: win
+        }, 800);
+        return false;
+    });
+});
 
                                                                            
 
